f957b3a360bec7d79e4fd3464725c4a3332772c8
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-02-24 18:31:44 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TaskManagerApplication-React.jsAndDjango
+
+This is Full-Stack Project with React.js for the frontend UI and Django Framework for the backend. 
+
+We are using Django for the structure design of the backend side and  frontend UI library React JS for the the frontend side. 
+This is a Task Manager desktop application.
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'corsheaders',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -99,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Whitelisting React port
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 
 # Internationalization
